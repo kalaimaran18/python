414fd2cf65ac5e3800fb36c45d490a2220c21608
kalaimaran18 <kalaimaranr18@gmail.com>
2025-08-16 21:49:17 +0530

    Add files via upload

---
+++ While loop.py
@@ -0,0 +1,28 @@
+#create while to print 1 to 5
+
+i=1
+while(i<=5):
+    print(i)
+    i=i+1
+
+# create output 10,20,30,...
+
+i=10
+while(i<=200):
+    print(i)
+    i=i+10
+
+
+# 10 natural number print in reverse order
+
+i=10
+while(i>0):
+    print(i)
+    i=i-1
+
+# print output 5,10,15,20
+
+i=5
+while(i<=60):
+    print(i)
+    i=i+5
