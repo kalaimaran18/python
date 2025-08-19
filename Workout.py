1f66bc0594d85846b4f6d86b956bce9be05ea95c
kalaimaran18 <kalaimaranr18@gmail.com>
2025-08-19 22:26:57 +0530

    Add files via upload

--- Workout.py
+++ Workout.py
@@ -18,3 +18,25 @@ words=strval.split()
 print(words[0].swapcase(),end=" ")
 for w in words [1::]:
     print(w, end=" ")
+
+
+
+'''
+Certain space separated numbers are passed as the input to the program. 
+The program must print only the even numbers in the reverse order of their occurrence in the input sequence.
+
+Input:
+
+10 99 23 12 78 81 102
+
+Output:
+
+102 78 12 10
+
+'''
+var=list(map(int,input().split(" ")))
+
+for i in var[::-1]:
+
+    if i%2==0:
+        print(i,end=" ")
