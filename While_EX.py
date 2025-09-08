5e0ad23ae3171cb62c65505088c446a8c80db399
R.Kalai Maran <kalaimaranr18@gmail.com>
2025-09-08 18:07:44 +0530

    Add files via upload

---
+++ While_EX.py
@@ -0,0 +1,24 @@
+# Task 1
+# Print 1 to 5 natural numbers using while Loop
+
+i=1
+while(i<6):
+    print(i,end=" ")
+    i=i+1
+
+
+# Task 2
+# Print the value of the Answer 10,20,....200
+i=10
+while(i<=200):
+    print(i,end=" ")
+    i=i+10
+
+
+# Task 3
+# Print the first 10 natural numbers reverse Order
+
+i=10
+while(i>0):
+    print(i,end=" ")
+    i=i-1
